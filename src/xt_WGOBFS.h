4b64f1443ea64f04f112414e611efc6952d79162
mrsobakin <68982655+mrsobakin@users.noreply.github.com>
2024-06-24 00:41:01 +0300

    Allow modifying keepalive packets drop probability

--- src/xt_WGOBFS.h
+++ src/xt_WGOBFS.h
@@ -6,8 +6,12 @@
 #define XT_MODE_OBFS   0
 #define XT_MODE_UNOBFS 1
 
+#define XT_WGOBFS_DEFAULT_KDC 205
+
 struct xt_wg_obfs_info {
     unsigned char mode;
+    unsigned char keepalive_drop_chance;
+    unsigned char is_kdc_default;
     char key[XT_WGOBFS_MAX_KEY_SIZE + 1];
     unsigned char chacha_key[XT_CHACHA_KEY_SIZE];  /* 256 bits chacha key */
 };
